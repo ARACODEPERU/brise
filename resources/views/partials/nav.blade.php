fa60d44bc8b889da5c952b6bb12509f3fae5516a
Yisus Web <connexion.jesus@gmail.com>
2026-04-06 00:46:31 -0500

    fvg

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -35,7 +35,7 @@
 
             <!-- Mobile menu button -->
             <div class="md:hidden flex items-center">
-                <button @click="open = !open" type="button" :class="{ 'text-gray-900': !atTop, 'text-white': atTop }" class="inline-flex items-center justify-center p-2 rounded-md focus:outline-none transition-colors">
+                <button @click="open = !open" type="button" :class="{ 'text-gray-900': !atTop, 'text-gray-900': atTop }" class="inline-flex items-center justify-center p-2 rounded-md focus:outline-none transition-colors">
                     <svg x-show="!open" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
